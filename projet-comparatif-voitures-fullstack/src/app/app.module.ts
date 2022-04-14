4d44f8616a0e89094cfcdc58eb0249303159461b
RanimElChami <ranim.el-chami@dauphine.eu>
2022-04-14 22:05:58 +0200

    Several components creation

--- projet-comparatif-voitures-fullstack/src/app/app.module.ts
+++ projet-comparatif-voitures-fullstack/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { CarAdvertisementAdminComponent } from './car-advertisement-admin/car-advertisement-admin.component';
+import { ReservationRecapComponent } from './reservation-recap/reservation-recap.component';
+import { ReservationConfirmationComponent } from './reservation-confirmation/reservation-confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AdminHomeComponent,
+    CarAdvertisementAdminComponent,
+    ReservationRecapComponent,
+    ReservationConfirmationComponent
   ],
   imports: [
     BrowserModule,
